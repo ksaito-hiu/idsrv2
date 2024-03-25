58dd2e498b17a8d41441502e3a2c98ba73909217
Kenji Saito <ksaito@do-johodai.ac.jp>
2024-03-26 02:45:07 +0900

    Google認証のためのAPIの枠組みだけ作った。

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './HelloWorld';
 export * from './identity/interaction/google/GoogleOIDC';
+export * from './identity/interaction/google/CreateGoogleHandler';
+export * from './identity/interaction/google/LoginGoogleHandler';
+
