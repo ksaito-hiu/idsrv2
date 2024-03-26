15b6fea2e17b4f75b2344713ece188763240cdb7
Kenji Saito <ksaito@do-johodai.ac.jp>
2024-03-26 17:41:03 +0900

    Google認証部分をcss-google-authパッケージに分離。

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 export * from './HelloWorld';
-export * from './identity/interaction/google/GoogleOIDC';
-export * from './identity/interaction/google/CreateGoogleHandler';
-export * from './identity/interaction/google/LoginGoogleHandler';
 
