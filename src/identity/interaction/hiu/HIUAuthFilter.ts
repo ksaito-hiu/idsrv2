c28e1a5a4ced7eb1e076a1d2447a58d4764cccc8
Kenji Saito <ksaito@do-johodai.ac.jp>
2024-05-29 19:51:30 +0900

    必要な機能の実現可能性が見えた。

--- src/identity/interaction/hiu/HIUAuthFilter.ts
+++ src/identity/interaction/hiu/HIUAuthFilter.ts
@@ -3,6 +3,16 @@ import { GoogleAuthFilter } from 'css-google-auth';
 export class HIUAuthFilter implements GoogleAuthFilter {
   async check(tokenSet: any): Promise<void> {
 console.log('GAHA: ',tokenSet);
+    const email = tokenSet.claims().email;
+    if (email.endsWith('@s.do-johodai.ac.jp')) {
+      console.log('HIUAuthFilter: student email=',email);
+    } else if (email.endsWith('@do-johodai.ac.jp')) {
+      console.log('HIUAuthFilter: email=',email);
+      //throw new Error('HIUAuthFilter: only students can be registered automaticaly. Please ask an administorator. email='+email);
+    } else {
+      console.log('HIUAuthFilter: invalid email=',email);
+      throw new Error('HIUAuthFilter: invalid email='+email);
+    }
     return;
   }
 }
--- src/identity/interaction/hiu/HIUPostGAccountGen.ts
+++ src/identity/interaction/hiu/HIUPostGAccountGen.ts
@@ -1,10 +1,34 @@
 import { PostGAccountGen } from 'css-google-auth';
+import type { WebIdStore } from '@solid/community-server';
 
 export class HIUPostGAccountGen implements PostGAccountGen {
+  private readonly webIdStore: WebIdStore;
+  private readonly webIdBase: string;
+
+  public constructor(webIdStore: WebIdStore, webIdBase: string) {
+    this.webIdStore = webIdStore;
+    this.webIdBase = webIdBase;
+  }
+
+  createWebIdFromEMail(email: string): string {
+    if (email.endsWith('@s.do-johodai.ac.jp')) {
+      return this.webIdBase+"s20"+email.substring(1,8)+'#me';
+    } else if (email.endsWith('@do-johodai.ac.jp')) {
+      return this.webIdBase+'f200088071#me'; // ちょっと自分だけ前提でテスト
+      console.log('HIUAuthFilter: email=',email);
+      //throw new Error('HIUPostGAccountGen: only students can be registered automaticaly. Please ask an administorator. email='+email);
+    } else {
+      console.log('HIUPostGAccountGen: invalid email=',email);
+      throw new Error('HIUPostGAccountGen: invalid email='+email);
+    }
+  }
+
   async handle(accountId: string, googleId: string, tokenSet: any): Promise<void> {
-console.log('GAHA1: ', accountId);
-console.log('GAHA2: ', googleId);
-console.log('GAHA3: ', tokenSet);
+    const email = tokenSet.claims().email;
+console.log('GAHA1: email=', email);
+    const webId = this.createWebIdFromEMail(email);
+console.log('GAHA2: webId=', webId);
+    const a = await this.webIdStore.create(webId, accountId);
     return;
   }
 }
