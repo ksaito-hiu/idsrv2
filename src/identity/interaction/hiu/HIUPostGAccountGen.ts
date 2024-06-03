d8a84b06b60496e0bf69a385b8bfd41bba2ac7ca
Kenji Saito <ksaito@do-johodai.ac.jp>
2024-06-03 18:41:48 +0900

    情報大専用の情報を保存するJsonResourceStorageを作ってWebIDのttlを配信できるようにした。

--- src/identity/interaction/hiu/HIUPostGAccountGen.ts
+++ src/identity/interaction/hiu/HIUPostGAccountGen.ts
@@ -1,21 +1,32 @@
 import { PostGAccountGen } from 'css-google-auth';
 import type { WebIdStore } from '@solid/community-server';
+import type { JsonResourceStorage } from '@solid/community-server';
+import type { HiuData } from './HIUProfileHandler';
 
 export class HIUPostGAccountGen implements PostGAccountGen {
   private readonly webIdStore: WebIdStore;
   private readonly webIdBase: string;
+  private readonly hiuStorage: JsonResourceStorage<HiuData>;
 
-  public constructor(webIdStore: WebIdStore, webIdBase: string) {
+  public constructor(webIdStore: WebIdStore, webIdBase: string, hiuStorage: JsonResourceStorage<HiuData>) {
     this.webIdStore = webIdStore;
     this.webIdBase = webIdBase;
+    this.hiuStorage = hiuStorage;
   }
 
-  createWebIdFromEMail(email: string): string {
+  createIdsFromEMail(email: string) {
     if (email.endsWith('@s.do-johodai.ac.jp')) {
-      return this.webIdBase+"s20"+email.substring(1,8)+'#me';
+      return {
+        webId: this.webIdBase+"s20"+email.substring(1,8)+'#me',
+        hiuId: "s20"+email.substring(1,8)
+      };
     } else if (email.endsWith('@do-johodai.ac.jp')) {
-      return this.webIdBase+'f200088071#me'; // ちょっと自分だけ前提でテスト
-      console.log('HIUAuthFilter: email=',email);
+console.log('GAHA: HIUAuthFilter, email=',email);
+      // ちょっと自分だけ前提でテスト
+      return {
+        webId: this.webIdBase+'f200088071#me',
+        hiuId: 'f200088071'
+      };
       //throw new Error('HIUPostGAccountGen: only students can be registered automaticaly. Please ask an administorator. email='+email);
     } else {
       console.log('HIUPostGAccountGen: invalid email=',email);
@@ -26,9 +37,10 @@ export class HIUPostGAccountGen implements PostGAccountGen {
   async handle(accountId: string, googleId: string, tokenSet: any): Promise<void> {
     const email = tokenSet.claims().email;
 console.log('GAHA1: email=', email);
-    const webId = this.createWebIdFromEMail(email);
+    const { webId, hiuId } = this.createIdsFromEMail(email);
 console.log('GAHA2: webId=', webId);
     const a = await this.webIdStore.create(webId, accountId);
+    await this.hiuStorage.set(hiuId, {hiuId, accountId, googleId});
     return;
   }
 }
--- src/identity/interaction/hiu/HIUProfileHandler.ts
+++ src/identity/interaction/hiu/HIUProfileHandler.ts
@@ -1,6 +1,13 @@
 import { HttpHandler } from '@solid/community-server';
 import { HttpHandlerInput } from '@solid/community-server';
 import { NotImplementedHttpError } from '@solid/community-server';
+import { JsonResourceStorage } from '@solid/community-server';
+
+export type HiuData = {
+  hiuId: string, // 北海道情報大学の学籍番号、教職員番号を拡張したやつ
+  accountId: string, // CSSのアカウントID
+  googleId: string // 重複するけどこちらにも記録
+};
 
 const profileTemplate = `@prefix foaf: <http://xmlns.com/foaf/0.1/>.
 @prefix solid: <http://www.w3.org/ns/solid/terms#>.
@@ -17,35 +24,55 @@ const profileTemplate = `@prefix foaf: <http://xmlns.com/foaf/0.1/>.
 `;
 const regex1 = /WEB_ID_TEMPLATE/g;
 const regex2 = /OIDC_ISSUER_TEMPLATE/g;
+const hiuIdRegexp = /\/people\/[sf]\d{9}/;
 
 /* 北海道情報大での使用を前提にしたWebIDのプロファイル情報を配信するHttpHandler */
 export class HIUProfileHandler extends HttpHandler {
   private readonly profileRoot: string;
   private readonly issuerUrl: string;
+  private readonly hiuStorage: JsonResourceStorage<HiuData>;
 
-  public constructor(profileRoot: string, issuerUrl: string) {
+  public constructor(profileRoot: string, issuerUrl: string, hiuStorage: JsonResourceStorage<HiuData>) {
     super();
     this.profileRoot = profileRoot;
     this.issuerUrl = issuerUrl;
+    this.hiuStorage = hiuStorage;
+  }
+
+  createHiuIdFromUrl(url: string): string {
+//console.log("GAHA: HIUProfileHandler#createHiuIdFromUrl: url=("+url+")");
+    if (url.length === 18) { // '/people/s202421000'.length === 18
+      const m = url.match(hiuIdRegexp);
+      if (m) {
+        return m[0].substring(8);
+      } else {
+        throw new Error('HIUProfileHandler: error1.');
+      }
+    }
+    throw new Error('HIUProfileHandler: error2.');
   }
 
   public async canHandle({ request }: HttpHandlerInput): Promise<void> {
-console.log("GAHA: HIUProfileHandler#canHandle: request.url=("+request.url+")");
     if (request.method !== 'GET' && request.method !== 'HEAD') {
       throw new NotImplementedHttpError('Only GET and HEAD requests are supported');
     }
     if (request.url == undefined) {
       throw new Error('can not handle.');
-    } else if (request.url.startsWith('/people/')) {
-      return;
     } else {
-      throw new Error('can not handle.');
+      const hiuId = this.createHiuIdFromUrl(request.url);
+      const a = await this.hiuStorage.get(hiuId);
+      if (a)
+        return;
+      throw new Error('HIUProfileHandler: can not handle.');
     }
   }
 
   public async handle({ request, response }: HttpHandlerInput): Promise<void> {
-console.log("GAHA: HIUProfileHandler#handle: request.url=("+request.url+")");
-    const id = 'f200088071'; // GAHA; TODO
+//console.log("GAHA: HIUProfileHandler#handle: request.url=("+request.url+")");
+    if (request.url == undefined) {
+      throw new Error('HIUProfileHandler: 404?');
+    }
+    const id = this.createHiuIdFromUrl(request.url);
     const webId = this.profileRoot + id + '#me';
     const bodyStr = profileTemplate.replace(regex1, webId).replace(regex2, this.issuerUrl);
     response.writeHead(200, { 'content-type': 'text/tutle' });
